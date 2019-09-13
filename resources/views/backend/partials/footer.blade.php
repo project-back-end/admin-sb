1e850c10c7b84ecbaaaf2b7ccd24f5d61f798060
NgoUyUong <ngouyoung@yahoo.com>
2019-09-13 12:30:18 +0700

    first commit

---
+++ resources/views/backend/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Make by your love <i class="fas fa-heart"></i> 2019</span>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/backend/partials/script.blade.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('node_modules/jquery.easing/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('node_modules/chart.js/dist/Chart.min.js') }}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
